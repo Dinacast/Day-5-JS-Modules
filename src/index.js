e55794e714e2cdd24c71898b46d6b0829ddb3196
Dina Cahyanti <124108993+Dinacast@users.noreply.github.com>
2024-11-01 15:10:52 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import square, { NAME, version } from "./modules/square";
+
+console.log(NAME);
+console.log(version);
+console.log(square(4));
---
+++ src/modules/square.js
@@ -0,0 +1,7 @@
+export const NAME = "square";
+
+export let version = "1.0";
+
+export default function square(number) {
+  return number * number;
+}
